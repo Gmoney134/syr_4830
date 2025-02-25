26f8b2ff77d3cdc72b2ed1e3b96e39c0dba6fe47
Grant Nichter <nichter.grant@gmail.com>
2025-02-25 14:21:27 -0600

    fix docker configs

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 export default function HomePage() {
   return (
     <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>Hello world</h1>
+      <h1>Hello world!</h1>
     </main>
   );
 }
